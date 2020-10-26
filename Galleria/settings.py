840d1abba2d1d97e7e920485a402cf6eb82da285
Noble <mungunoble@gmail.com>
2020-10-26 23:52:05 +0300

    removed static dirs

--- Galleria/settings.py
+++ Galleria/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'photos/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'photos/static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
