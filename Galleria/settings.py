95a6658d88a91206497dd1521b5869e83fa11cf5
Noble Mungu <mungunoble@gmail.com>
2023-03-09 22:13:40 +0300

    collect static files

--- Galleria/settings.py
+++ Galleria/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos'
+    'photos',
+    'whitenoise.runserver_nostatic',
+    
 ]
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -129,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles') 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'photos/static'),
